953613b7927c59c02efbcc0f66500446d08790ae
Albrecht Lohofener <albrechtloh@gmx.de>
2016-07-29 09:05:37 +0200

    Added read of input device from the INI-file

--- gui_3/gui.cpp
+++ gui_3/gui.cpp
@@ -511,12 +511,13 @@ void	RadioInterface::init_your_gui (void) {
 /**
   *	we now handle the settings as saved by previous incarnations.
   */
-    //setDevice 		("dabstick");
-    setDevice 		("rtl_tcp");
 	QString h		=
 	           dabSettings -> value ("device", "no device"). toString ();
 	if (h == "no device")	// no autostart here
 	   autoStart = false;
+    //setDevice 		("dabstick");
+    //setDevice 		("rtl_tcp");
+    setDevice 		(h);
 
 	h		= dabSettings -> value ("channel", "12C"). toString ();
 	
